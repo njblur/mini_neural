correct softmax activation & mnist works!

mnist.py
@@ -1,24 +1,25 @@
 import mnist_reader
 import numpy as np
 import neural
-batch_size = 20
+batch_size = 100
 label_num = 10
 data = mnist_reader.load_mnist(train_dir='data')
 train = data.train
+test = data.test
 
 size = train.num_examples
 idx = range(batch_size)
 print size
 
 
-l1 = neural.layer(784,50,"liner")
-l2 = neural.layer(50,10,"softmax")
+l1 = neural.layer(784,100,"liner")
+l2 = neural.layer(100,10,"softmax")
 
-net = neural.network("softmax",learning_rate=0.0001)
+net = neural.network("softmax",learning_rate=0.00015)
 net.add_layer(l1)
 net.add_layer(l2)
 
-for _ in range(5):
+for _ in range(2*size//batch_size):
     images,labels = train.next_batch(batch_size)
     l = np.zeros([label_num,batch_size])
     images = images.T
@@ -27,6 +28,16 @@ for _ in range(5):
     loss = net.learn(images,l)
 
     print loss
+test_size = len(test.images)
+correct = 0
+for _ in range(test_size):
+    images,labels = test.next_batch(1)
+    images = images.T
+    o = net.eval(images)
+    n = np.argmax(o,axis=0)
+    print n
+    print labels
+    if(n[0] == labels[0] ):
+        correct += 1
 
-print l1.weights
-print l2.weights
+print "{} correct, rate is {}".format(correct,correct*1.0/test_size)

neural.py
@@ -11,9 +11,8 @@ def prime_sigmoid(x):
 def softmax(x):
     maxx = np.max(x,axis=0,keepdims=0)
     regx = x-maxx
-    print regx
     exp = np.exp(regx)
-    sum = np.sum(exp)
+    sum = np.sum(exp,axis=0,keepdims=0)
     y = exp/sum
     return y
 def prime_softmax(x):