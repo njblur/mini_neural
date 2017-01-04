5b2b3032539fe8fb58f5bd0b8c7a7bd25eb58374
hujianwu <hujianwu@caredear.com>
2017-01-04 17:31:58 +0800

    tf lstm,not working

--- tf_charnn.py
+++ tf_charnn.py
@@ -4,30 +4,31 @@ data = open("input.txt").read()
 vocab = list(set(data))
 char_to_idx = {c:i for i,c in enumerate(vocab) }
 idx_to_char = {i:c for i,c in enumerate(vocab) }
-hidden_size = 10
+hidden_size = 5
 vocab_size = len(vocab)
 data_size = len(data)
 
-learning_rate = 0.1
-epoch = 1
+learning_rate = 0.5
+epoch = 1000
 
 
 inputs = tf.placeholder(shape=[1,vocab_size], dtype=tf.float32)
 targets = tf.placeholder(shape=[1,vocab_size], dtype=tf.float32)
 cell = tf.nn.rnn_cell.BasicRNNCell(hidden_size)
-softmax_weights = tf.Variable(tf.random_uniform([vocab_size,hidden_size]))
+softmax_weights = tf.Variable(tf.random_uniform([vocab_size,hidden_size],dtype=tf.float32,maxval=0.01,minval=0))
 softmax_bias = tf.Variable(tf.zeros([vocab_size,1]))
 init_state = cell.zero_state(1,tf.float32)
-# state = init_state
+state = init_state
 
 h,state = cell(inputs,init_state)
 
-out = tf.matmul(softmax_weights,h,transpose_b=True)+softmax_bias
+out = tf.matmul(softmax_weights,h,transpose_b=True) #+softmax_bias
 out_max = tf.nn.softmax(out,dim=0)
 out_index = tf.argmax(out_max,0)
-loss = tf.nn.sparse_softmax_cross_entropy_with_logits(tf.reshape(out_max,[8,1]),tf.arg_max(targets,0))
+# y = tf.arg_max(targets,0)
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(tf.reshape(out_max,[1,vocab_size]),out_index)
 
-train = tf.train.AdagradOptimizer(0.1).minimize(loss)
+train = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
 in_vec = np.zeros([vocab_size,1])
 out_vec = np.zeros([vocab_size,1])
@@ -38,18 +39,24 @@ with tf.Session() as sess:
         nstate = sess.run(init_state)
         total_loss = 0
         for j in xrange(data_size-1):
-            in_char = data[i]
+            in_char = data[j]
             in_idx = char_to_idx[in_char]
-            out_char = data[i+1]
+            out_char = data[j+1]
             out_idx = char_to_idx[out_char]
             in_vec[:,:] = 0
             in_vec[in_idx,0] = 1.0
             out_vec[:,:] = 0
             out_vec[out_idx,0] = 1.0
-            nstate,l,t,m = sess.run([state,loss,train,out_max],feed_dict={inputs:in_vec.T,targets:out_vec.T,init_state:nstate})
-            print m.shape
+            nstate,l,t,m,idx = sess.run([state,loss,train,out_max,out_index],feed_dict={inputs:in_vec.T,targets:out_vec.T,state:nstate})
+            print("in {} out {}".format(in_vec,out_vec))
+            # print nstate
+            # print out_vec
+            # print m
+            # print l
+            # print idx
             total_loss += l
         print("{} loss after {} iter ".format(total_loss,i))
+        # print softmax_bias.eval()
     start = data[0]
     start_idx = char_to_idx[start]
     start_vec = np.zeros([vocab_size, 1])
