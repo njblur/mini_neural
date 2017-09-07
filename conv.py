218047b240b2dcc995eef2dd899bc46d21c4cead
hujianwu <jianwu.hu@gmail.com>
2017-09-07 17:48:17 +0800

    implement batch,not fully working yet

--- conv.py
+++ conv.py
@@ -41,38 +41,40 @@ class conv2d:
         padding = self.padding
         stride = self.stride
         self.x = x
-        ih,iw,ic = x.shape
+        b,ih,iw,ic = x.shape
         h,w,c = ih+2*padding,iw+2*padding,ic
-        self.expand = np.zeros((h,w,c),dtype='float')
-        self.expand[padding:-padding,padding:-padding,:] = x
+        self.expand = np.zeros((b,h,w,c),dtype='float')
+        self.expand[:,padding:-padding,padding:-padding,:] = x
         fh,fw,fic,foc = self.filter.shape
         assert(c == fic)
         target_h = (h-fh)/stride+1
         target_w = (w-fw)/stride+1
-        garden = np.zeros(shape=[target_h,target_w,fh,fw,c])
+        garden = np.zeros(shape=[b,target_h,target_w,fh,fw,c])
         for i in range(target_h):
             for j in range(target_w):
-                garden[i,j,:,:,:] = self.expand[i*stride:i*stride+fh,j*stride:j*stride+fw,:]
+                garden[:,i,j,:,:,:] = self.expand[:,i*stride:i*stride+fh,j*stride:j*stride+fw,:]
 
-        self.img2col = garden.reshape(target_h*target_w,fh*fw*c)
+        self.img2col = garden.reshape(b,target_h*target_w,fh*fw*c)
         y = self.img2col.dot(self.filter_linear)+self.bias
-        return y.reshape(target_h,target_w,foc)
+        return y.reshape(b,target_h,target_w,foc)
     def backward(self,dy):
-        dy_col = dy.reshape(-1,dy.shape[-1])
-        self.dbias = np.sum(dy_col,axis=0)
-        dfilter_linear = self.img2col.T.dot(dy_col)
+        b,th,tw,tc = dy.shape
+        dy_col = dy.reshape(b,-1,dy.shape[-1])
+        self.dbias = np.mean(np.sum(dy_col,axis=0),axis=0)
+        dfilter_linear = np.matmul(self.img2col.transpose(0,2,1),dy_col)
+        dfilter_linear = np.mean(dfilter_linear,axis=0)
         self.dfilter = dfilter_linear.reshape(self.filter.shape)
-        dimg2col = dy_col.dot(self.filter_linear.T)
+        dimg2col = np.matmul(dy_col,self.filter_linear.T)
         fh,fw,fci,fco = self.filter.shape
-        th,tw,tc = dy.shape
-        dgarden = dimg2col.reshape(th,tw,fh,fw,fci)
+
+        dgarden = dimg2col.reshape(b,th,tw,fh,fw,fci)
         dexpand = np.zeros_like(self.expand)
         stride = self.stride
         padding = self.padding
         for i in range(th):
             for j in range(tw):
-                dexpand[i*stride:i*stride+fh,j*stride:j*stride+fw,:] += dgarden[i,j,:,:,:]
-        return dexpand[padding:-padding,padding:-padding,:]
+                dexpand[:,i*stride:i*stride+fh,j*stride:j*stride+fw,:] += dgarden[:,i,j,:,:,:]
+        return dexpand[:,padding:-padding,padding:-padding,:]
     def apply_gradients(self,learning_rate):
         self.m_bias = self.m_bias*0.9 + self.dbias*0.1
         self.m_filter = self.m_filter*0.9 + self.dfilter*0.1
--- mnist_conv.py
+++ mnist_conv.py
@@ -4,7 +4,7 @@ import IPython
 import mnist_reader
 import neural
 import conv
-batch_size = 20
+batch_size = 1
 label_num = 10
 data = mnist_reader.load_mnist(train_dir='data',reshape=False)
 train = data.train
@@ -28,57 +28,56 @@ l2 = neural.layer(hidden,10,"softmax",weight_decay=0.0)
 
 loop = size//batch_size
 
-epoch = 10
+epoch = 1
 learning_rate = 0.005
 for k in range(epoch):
     for j in range(loop):
         images,labels = train.next_batch(batch_size)
-        l = np.zeros([batch_size,label_num])
-        l[idx,[labels]] = 1
-        for i in range(batch_size):
-            data = images[i]
-            label = l[i].reshape(-1,1)
-            conv_out0 = conv_layer0.forward(data)
-            reludata0 = relu0.forward(conv_out0)
-            conv_out = conv_layer.forward(reludata0)
-            reludata = relu.forward(conv_out)
-            linear = reludata.reshape(-1,1)
-            l1_out = l1.forward(linear)
-            dropoutdata0 = dropout0.forward(l1_out)
-            l2_out = l2.forward(dropoutdata0)
-            loss = neural.softmax_loss(l2_out,label)
-            if(i == 10):
-                print 'loss is ' + str(loss) + '  '+str(j*100.0/loop)+'%'
-            dloss = neural.prime_softmax_loss(l2_out,label)
-            dl2_out = l2.backward(dloss)
-            ddropout0 = dropout0.backward(dl2_out)
-            dlinear = l1.backward(ddropout0)
-            dreluout = dlinear.reshape(reludata.shape)
-            dconv_out = relu.backward(dreluout)
-            ddata = conv_layer.backward(dconv_out)
-            drelu0 = relu0.backward(ddata)
-            dconv_out0 = conv_layer0.backward(drelu0)
-            conv_layer.apply_gradients(learning_rate)
-            conv_layer0.apply_gradients(learning_rate)
-            l1.apply_gradients(learning_rate)
+        l = np.zeros([label_num,batch_size])
+        l[[labels],idx] = 1
+        data = images
+        labels = l.reshape(batch_size,-1).T
+        conv_out0 = conv_layer0.forward(data)
+        reludata0 = relu0.forward(conv_out0)
+        conv_out = conv_layer.forward(reludata0)
+        reludata = relu.forward(conv_out)
+        linear = reludata.reshape(batch_size,-1).transpose(1,0) # my bad, our basic neural cell need Dxbatch input
+        l1_out = l1.forward(linear)
+        dropoutdata0 = dropout0.forward(l1_out)
+        l2_out = l2.forward(dropoutdata0)
+        loss = neural.softmax_loss(l2_out,labels).sum()/batch_size
+        print 'loss is ' + str(loss) + '  '+str(j*100.0/loop)+'%'
+        IPython.embed()
+        dloss = neural.prime_softmax_loss(l2_out,labels)
+        dl2_out = l2.backward(dloss)
+        ddropout0 = dropout0.backward(dl2_out)
+        dlinear = l1.backward(ddropout0)
+        dlinear = dlinear.T
+        dreluout = dlinear.reshape(reludata.shape)
+        dconv_out = relu.backward(dreluout)
+        ddata = conv_layer.backward(dconv_out)
+        drelu0 = relu0.backward(ddata)
+        dconv_out0 = conv_layer0.backward(drelu0)
+        conv_layer.apply_gradients(learning_rate)
+        conv_layer0.apply_gradients(learning_rate)
+        l1.apply_gradients(learning_rate)
     if (k > 0):
         learning_rate =0.002
 
     correct = 0
     test_size = test.num_examples
-    for i in range(test_size):
-        images,labels = test.next_batch(1)
-        data = images[0]
-        label = labels[0]
+    for i in range(test_size//batch_size):
+        images,labels = test.next_batch(batch_size)
+        data = images
+        label = labels
         conv_out0 = conv_layer0.forward(data)
         reludata0 = relu0.forward(conv_out0)
         conv_out = conv_layer.forward(reludata0)
         reludata = relu.forward(conv_out)
-        linear = conv_out.reshape(-1,1)
+        linear = conv_out.reshape(-1,batch_size)
         l1_out = l1.forward(linear)
         l2_out = l2.forward(l1_out)
         n = np.argmax(l2_out,axis=0)
-        if(n[0] == label ):
-            correct += 1
+        correct += np.count_nonzero(n == label )
     print "{} correct, rate is {}".format(correct,correct*1.0/test_size)
     # IPython.embed()
